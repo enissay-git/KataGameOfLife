304afdefd236608aa2cf88c0c3e9bac9487f7d7e
enissay <yassine.aroua.dev@gmail.com>
2018-02-02 18:18:30 +0100

    Create the interface of the game of life algorithm

---
+++ GameOfLife/GameOfLifeInterface.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameOfLife
+{
+    interface GameOfLifeInterface
+    {
+
+        int CountLivingNeighbours (int row, int column);
+
+        bool IsAlive(int row, int column);
+
+        void NextGenerationGrid();
+
+        void InitFirstGenerationGrid(int heigth, int width, int[,] livingCellsPosition);
+
+    }
+}
