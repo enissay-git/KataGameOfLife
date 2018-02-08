7fef5f11130f29d48e9a574a0c92fb51896fff4d
enissay <yassine.aroua.dev@gmail.com>
2018-02-08 11:08:45 +0100

    Suppression de commentaire

--- GameOfLifeTest/GameOfLifeTests.cs
+++ GameOfLifeTest/GameOfLifeTests.cs
@@ -8,12 +8,6 @@ namespace GameOfLifeTest
     [TestClass]
     public class GameOfLifeTests
     {
-        /*
-         * 1. Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
-         * 2. Any live cell with more than three live neighbours dies, as if by overcrowding.
-         * 3. Any live cell with two or three live neighbours lives on to the next generation.
-         * 4. Any dead cell with exactly three live neighbours becomes a live cell.
-        */
         private IGameOfLifeService myInterface;
 
         private void InitTest ()
